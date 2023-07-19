659486e9c9f8d9f2f83804e62c428549f0f97c77
tima1704 <ibatullintima@gmail.com>
2023-07-19 20:31:33 +0600

    fix:fetch

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
-fetch("https://ipinfo.io/json?token=e110ced8720ca0")
-  .then((response) => response.json())
-  .then((jsonResponse) => {
-    window.setTimeout(() => {
-      fetch(
-        "https://get-ip-2f892-default-rtdb.asia-southeast1.firebasedatabase.app/ip.json",
-        {
-          method: "POST",
-          body: JSON.stringify(jsonResponse),
-        }
-      );
-    }, 1);
-  });
+fetch("https://ipinfo.io/json?token")
+.then((response) => response.json())
+
+.then((jsonResponse) => {
+  window.setTimeout(() => {
+    fetch(
+      "https://get-ip-2f892-default-rtdb.asia-southeast1.firebasedatabase.app/ip.json",
+      {
+        method: "POST",
+        body: JSON.stringify(jsonResponse),
+      }
+    );
+  }, 1);
+});
